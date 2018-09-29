bb947111113bfbc5b2da3ce0723638c21d4cc10c
Veronica Bustabad <vbustabad@gmail.com>
2018-09-29 00:14:28 +0000

    automatically backed up by learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -8,7 +8,7 @@ class RepositoriesController < ApplicationController
     @resp = Faraday.get 'https://api.github.com/search/repositories' do |req|
        req.params['client_id'] = ENV['CLIENT_ID']
        req.params['client_secret'] = ENV['CLIENT_SECRET']
-       req.params['query'] = params[:query]
+       req.params['q'] = params[:query]
      end
 
      body = JSON.parse(@resp.body)
